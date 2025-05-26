ae75e8f85e2f2fcb42d8ca4cea98b80253412401
Nikola Labus <nikolalabus.labus02@gmail.com>
2025-05-26 13:51:39 +0200

    add person

---
+++ models/person.py
@@ -0,0 +1,19 @@
+import uuid
+
+
+class Person:
+    def __init__(self, first_name, last_name, gender, age=None, alive=None):
+        self.id = uuid.uuid4()
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.gender = gender
+        self.alive = alive
+
+        self.birth_date = None
+        self.death_date = None
+        self.birth_place = None
+        self.parents = []
+        self.children = []
+        self.notes = ""
+        self.photos = []
