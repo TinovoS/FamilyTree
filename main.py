8f0f43ac34df02d6f78a23074aefd237f25a244c
Nikola Labus <nikolalabus.labus02@gmail.com>
2025-05-30 16:00:15 +0200

    ui partially done

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow, QStackedWidget
+
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QApplication, QMainWindow, QStackedWidget, QSplitter
 from view.ui import ViewUI, FormUI
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("FamilyTree")
-        self.resize(1024, 768)
+        self.resize(1440, 900)
 
-        self.central_stack = QStackedWidget()
-        self.setCentralWidget(self.central_stack)
+        main_splitter = QSplitter(Qt.Orientation.Horizontal)
 
-        self.form_ui = FormUI()
-        self.view_ui = ViewUI()
+        form_ui = FormUI()
+        view_ui = ViewUI()
+        main_splitter.addWidget(form_ui)
+        main_splitter.addWidget(view_ui)
 
-        self.central_stack.addWidget(self.form_ui)
-        self.central_stack.addWidget(self.view_ui)
+        main_splitter.setSizes([400, 1200])
+        self.setCentralWidget(main_splitter)
         #todo
-        #self.show_tree_view()
 
     def show_tree_view(self):
         #todo
@@ -28,7 +30,7 @@ class MainWindow(QMainWindow):
         #todo
         #if person_id:
         #    self.form_ui.load_person_data(person_id)
-        self.central_stack.setCurrentIndex(1)
+        self.central_stack.setCurrentIndex(0)
 
 def main():
     app = QApplication(sys.argv)
--- view/ui.py
+++ view/ui.py
@@ -1,15 +1,79 @@
-from PyQt6.QtWidgets import QMainWindow, QTreeView, QFormLayout, QLineEdit, QGraphicsView
+from PyQt6.QtWidgets import QMainWindow, QTreeView, QFormLayout, QLineEdit, QGraphicsView, QWidget, QVBoxLayout, QLabel, \
+    QPushButton
 
-class FormUI(QMainWindow):
+
+class FormUI(QWidget):
     def __init__(self):
         super().__init__()
-        form = QFormLayout()
-        form.addRow("Say Something: ", QLineEdit())
-        #todo
 
-class ViewUI(QMainWindow):
+        form_layout = QFormLayout()
+
+        form_layout.addRow(QLabel("<b>Personal Information</b>"))
+        self.first_name = QLineEdit()
+        self.last_name = QLineEdit()
+        self.birth_date = QLineEdit()
+        self.death_date = QLineEdit()
+
+        form_layout.addRow("First Name:", self.first_name)
+        form_layout.addRow("Last Name:", self.last_name)
+        form_layout.addRow("Birth Date:", self.birth_date)
+        form_layout.addRow("Death Date:", self.death_date)
+
+        form_layout.addRow(QLabel("<b>Relationships</b>"))
+        self.parents = QLineEdit()
+        self.spouses = QLineEdit()
+        self.children = QLineEdit()
+
+        form_layout.addRow("Parents:", self.parents)
+        form_layout.addRow("Spouses:", self.spouses)
+        form_layout.addRow("Children:", self.children)
+
+        form_layout.addRow(QLabel("<b>Actions</b>"))
+        self.save_btn = QPushButton("Save Person")
+        self.clear_btn = QPushButton("Clear Form")
+        form_layout.addRow(self.save_btn)
+        form_layout.addRow(self.clear_btn)
+
+        self.setLayout(form_layout)
+
+        self.setStyleSheet("""
+                    QWidget {
+                        padding: 15px;
+                        border-right: 1px solid #dee2e6;
+                    }
+                    QFormLayout QLabel {
+                        background: transparent;
+                        color: #495057;
+                    }
+                    QLabel[text^="<b>"] {
+                        font-weight: bold;
+                        margin-top: 15px;
+                        color: #495057;
+                    }
+                """)
+
+        self.save_btn.clicked.connect(self.save_person)
+        self.clear_btn.clicked.connect(self.clear_form)
+
+    def save_person(self):
+        print("Saving person:", self.first_name.text(), self.last_name.text())
+
+    def clear_form(self):
+        self.first_name.clear()
+        self.last_name.clear()
+        self.birth_date.clear()
+        self.death_date.clear()
+        self.parents.clear()
+        self.spouses.clear()
+        self.children.clear()
+
+
+class ViewUI(QWidget):
     def __init__(self):
         super().__init__()
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+
         self.treeView = QGraphicsView()
-        self.setCentralWidget(self.treeView)
+        layout.addWidget(self.treeView)
         #todo
